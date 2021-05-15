e4ac49cf0c07831d2775a694b310647ac4ddd2cd
Nikita Kovrigin <nikita.kovrigin@peaks.com>
2021-05-15 16:44:30 +0200

    added uuid for game

--- demo/src/main/java/com/example/demo/core/Game.java
+++ demo/src/main/java/com/example/demo/core/Game.java
@@ -1,13 +1,21 @@
 package com.example.demo.core;
 
+import java.util.UUID;
+
 public class Game {
 
     private final Board board;
+    final private String id;
     public Game(){
         this.board = new Board();
+        this.id = UUID.randomUUID().toString();
     }
 
     Board getBoard(){
         return this.board;
+    }
+
+    String getId(){
+        return this.id;
     }
 }
