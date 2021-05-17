cde32dd136723b3180055a66c90f4838619ac11f
Nikita Kovrigin <nikita.kovrigin@peaks.com>
2021-05-17 13:51:13 +0200

    tests for gameRepository

---
+++ demo/src/test/java/com/example/demo/repository/GameRepositoryTests.java
@@ -0,0 +1,52 @@
+package com.example.demo.repository;
+
+import com.example.demo.dbaccess.domain.Game;
+import com.example.demo.dbaccess.repository.GameRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class GameRepositoryTests {
+
+    @Autowired
+    private GameRepository gameRepository;
+    String gameId;
+
+    @BeforeAll
+    public void initGame() {
+        Game game = new Game();
+        gameId = game.getId();
+        gameRepository.save(game);
+    }
+
+    @Test
+    public void testCheckSizeOfRepo(){
+        Assertions.assertEquals(1, gameRepository.getAllGameIDs().size());
+    }
+
+    @Test
+    public void testCheckGameId(){
+        Assertions.assertTrue(gameRepository.getAllGameIDs().contains(gameId));
+    }
+
+    @Test
+    public void testFindById(){
+        Assertions.assertSame(gameRepository.findById(gameId).getId(), gameId);
+    }
+
+    @Test
+    public void testSave(){
+        Game newGame = new Game();
+        gameRepository.save(newGame);
+        Assertions.assertTrue(gameRepository.getAllGameIDs().contains(newGame.getId()));
+    }
+
+}
